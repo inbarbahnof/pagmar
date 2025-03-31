61b848cc0029b85c463ab6de65a0aa344720b372
inbarbahnof <158153825+inbarbahnof@users.noreply.github.com>
2025-03-31 13:00:31 +0300

    added camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 5, - 10);
+    }
+}
