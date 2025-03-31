1b49dc0754e10b7e0a4eaed7eaa19628bf533f46
inbarbahnof <158153825+inbarbahnof@users.noreply.github.com>
2025-03-31 13:05:49 +0300

    camera follow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,6 +6,6 @@ public class CameraFollow : MonoBehaviour
     
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 5, - 10);
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 1.4f, - 10);
     }
 }
