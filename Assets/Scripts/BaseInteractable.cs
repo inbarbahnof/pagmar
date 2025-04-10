eb4c6b598b1e0e0dade6514a192315b38121636d
inbarbahnof <158153825+inbarbahnof@users.noreply.github.com>
2025-04-10 16:37:00 +0300

    interactible interface

---
+++ Assets/Scripts/BaseInteractable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class BaseInteractable : MonoBehaviour, IInteractable
+{
+    [SerializeField] private GameObject highlightEffect;
+    protected bool readyToInteract = false;
+
+    public void SetHighlight(bool isHighlighted)
+    {
+        highlightEffect.SetActive(isHighlighted);
+        readyToInteract = isHighlighted;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SetHighlight(true);
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SetHighlight(false);
+        }
+    }
+
+    public abstract void Interact();
+}
