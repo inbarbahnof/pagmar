5033f19b8e471be5441a953a39c11019c181b60b
RuthSchiller <ruth.schiller1@mail.huji.ac.il>
2025-06-09 13:08:45 +0300

    text prompts

--- Assets/Scripts/TextTrigger.cs
+++ Assets/Scripts/TextTrigger.cs
@@ -4,23 +4,33 @@ using UnityEngine;
 public class TextTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject text;
-    private float showTime = 5f;
+    [SerializeField] private GameObject secondText;
+    [SerializeField] private float showTime = 5f;
     private bool _showing = false;
+    private int _textsShown = 0;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!_showing && other.CompareTag("Player"))
         {
             _showing = true;
-            StartCoroutine(ShowTxtForTime());
+            _textsShown++;
+            StartCoroutine(ShowTxtForTime(text));
         }
     }
 
-    private IEnumerator ShowTxtForTime()
+    private IEnumerator ShowTxtForTime(GameObject txt)
     {
-        text.SetActive(true);
+        txt.SetActive(true);
         yield return new WaitForSeconds(showTime);
-        text.SetActive(false);
+        txt.SetActive(false);
         _showing = false;
+        if (secondText != null && _textsShown == 1)
+        {
+            _showing = true;
+            _textsShown++;
+            StartCoroutine(ShowTxtForTime(secondText));
+        }
+        else _textsShown = 0;
     }
 }
