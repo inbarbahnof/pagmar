3a59e8893111fb58008a56ab726fd1545a057d9a
RuthSchiller <ruth.schiller1@mail.huji.ac.il>
2025-06-09 08:24:39 +0300

    fade out adjustment

--- Assets/Scripts/CameraFade.cs
+++ Assets/Scripts/CameraFade.cs
@@ -1,43 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CameraFade : MonoBehaviour
 {
     [SerializeField] private Image fade;
+    [SerializeField] private float duration = 3f;
     public Color startColor = new Color(0, 0, 0, 0);
     public Color endColor = new Color(0, 0, 0, 1);
 
     private Coroutine _fadeOutCoroutine;
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            FadeOutOverTime(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            FadeOutOverTime(false);
+        }
+    }
+
     public void FadeOutOverTime(bool reverse = false)
     {
-        float value = Mathf.Clamp01(0.75f);
-
         if (_fadeOutCoroutine != null)
             StopCoroutine(_fadeOutCoroutine);
 
-        _fadeOutCoroutine = StartCoroutine(LerpColor(value, reverse));
+        _fadeOutCoroutine = StartCoroutine(LerpColor(reverse));
     }
 
-    private IEnumerator LerpColor(float targetValue, bool reverse)
+    private IEnumerator LerpColor(bool reverse)
     {
-        float duration = 3f;
+        yield return new WaitForSecondsRealtime(1f);
         float time = 0f;
 
-        Color fromColor = reverse
-            ? Color.Lerp(startColor, endColor, targetValue)
-            : fade.color;
-
-        Color toColor = reverse
-            ? startColor
-            : Color.Lerp(startColor, endColor, targetValue);
+        Color fromColor = reverse ? endColor : startColor;
+        Color toColor   = reverse ? startColor : endColor;
 
         while (time < duration)
         {
             time += Time.unscaledDeltaTime;
-            float t = time / duration;
-            fade.color = Color.Lerp(fromColor, toColor, t);
+            float t = Mathf.Clamp01(time / duration);
+            float easedT = Mathf.SmoothStep(0f, 1f, t); // Smooth easing
+            fade.color = Color.Lerp(fromColor, toColor, easedT);
             yield return null;
         }
 
