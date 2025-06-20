2d15ed97352005856131c9bd523d6331be5591db
RuthSchiller <ruth.schiller1@mail.huji.ac.il>
2025-06-20 20:23:49 +0300

    Update EventTrigger.cs

--- Assets/Scripts/EventTrigger.cs
+++ Assets/Scripts/EventTrigger.cs
@@ -4,9 +4,11 @@ using UnityEngine.Events;
 public class EventTrigger : MonoBehaviour
 {
     [SerializeField] private UnityEvent triggerAction;
+    [SerializeField] private bool turnOffAfterTrigger;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player")) triggerAction?.Invoke();
+        if (turnOffAfterTrigger) gameObject.SetActive(false);
     }
 }
