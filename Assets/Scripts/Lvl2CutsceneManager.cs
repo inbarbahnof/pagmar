e08d909bd93e015934b6c0f33d9d08d967d66ae7
RuthSchiller <ruth.schiller1@mail.huji.ac.il>
2025-06-28 15:37:33 +0300

    lvl 2 cutscene fix

---
+++ Assets/Scripts/Lvl2CutsceneManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class Lvl2CutsceneManager : MonoBehaviour
+{
+        [SerializeField] private PlayableDirector sequence;
+        [SerializeField] private PlayerMove _playerMove;
+        
+        private bool _dogInPlace;
+        private bool _playerInPlace;
+        
+        public void UpdateDogInPlace()
+        {
+                _dogInPlace = true;
+                StartCutscene();
+        }
+        
+        public void UpdatePlayerInPlace()
+        {
+                _playerInPlace = true;
+                _playerMove.SetCanMove(false);
+                StartCutscene();
+        }
+
+        private void StartCutscene()
+        {
+                if (_playerInPlace && _dogInPlace && (sequence.state != PlayState.Playing))
+                {
+                        sequence.Play();
+                }
+        }
+}
