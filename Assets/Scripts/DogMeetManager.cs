139c211b5661ef273fa5c99230db1604c21b5023
inbarbahnof <158153825+inbarbahnof@users.noreply.github.com>
2025-07-09 11:36:32 +0300

    tutorial

--- Assets/Scripts/DogMeetManager.cs
+++ Assets/Scripts/DogMeetManager.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using DG.Tweening;
+using Dog;
+using Targets;
 using UnityEngine;
 using UnityEngine.Playables;
 
@@ -10,6 +12,11 @@ public class DogMeetManager : MonoBehaviour
     [SerializeField] private PlayableDirector ghostieSequence;
     [SerializeField] private PlayerMove playerMove;
     [SerializeField] private GameObject playerBlockCollider;
+    
+    [Header("Dog Come In")]
+    [SerializeField] private DogActionManager _dog;
+    [SerializeField] private WantFoodTarget _target;
+    
     private bool _playerHiding;
     
     public void ShowDogSequence()
@@ -19,6 +26,15 @@ public class DogMeetManager : MonoBehaviour
         // play sequence to pan camera right and move dog
         dogSequence.Play();
         // on camera pan stop allow player controls and show 'hide' prompt
+    }
+
+    public void DogComeIn()
+    {
+        _dog.gameObject.SetActive(true);
+        
+        TargetGenerator.instance.SetWantFoodTarget(_target);
+        _dog.SetWantsFood(true);
+        _dog.Running(true);
     }
 
     public void PlayerHiding()
