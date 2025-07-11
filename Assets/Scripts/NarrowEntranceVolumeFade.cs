d3fd4714cef0f2871c3b4956e2878271e5ef4787
inbarbahnof <158153825+inbarbahnof@users.noreply.github.com>
2025-07-11 20:36:03 +0300

    narrow entrance fix

---
+++ Assets/Scripts/NarrowEntranceVolumeFade.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class NarrowEntranceVolumeFade : MonoBehaviour
+{
+    [SerializeField] private GameObject _faderGameObject;
+    
+    private IObjectFader _fader;
+
+    private void Start()
+    {
+        if (_faderGameObject != null) _fader = _faderGameObject.GetComponent<IObjectFader>();
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _fader?.FadeOutOverTime(true);
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _fader?.FadeOutOverTime();
+        }
+    }
+}
