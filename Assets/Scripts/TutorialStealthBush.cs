fd86ba589c2259f60051c18c168a05f7a78b5eb7
RuthSchiller <ruth.schiller1@mail.huji.ac.il>
2025-07-13 23:57:05 +0300

    tutorial cutscene fixes
    girl locks in bush only when 2nd cutscene is triggered, bush glow fix

---
+++ Assets/Scripts/TutorialStealthBush.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class TutorialStealthBush : MonoBehaviour
+{
+    [SerializeField] private DogMeetManager _manager;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+        _manager.PlayerHiding(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+        _manager.PlayerHiding(false);
+    }
+}
