7f491381f24653dab733629b4442abc8f508af28
inbarbahnof <158153825+inbarbahnof@users.noreply.github.com>
2025-07-13 17:37:57 +0300

    bug lvl1

--- Assets/Scripts/Interactables/GhostiesCage1Obstacle.cs
+++ Assets/Scripts/Interactables/GhostiesCage1Obstacle.cs
@@ -1,18 +1,32 @@
+using System;
 using Ghosts;
 using UnityEngine;
+using UnityEngine.Rendering.VirtualTexturing;
 
 namespace Interactables
 {
     public class GhostiesCage1Obstacle : Obstacle
     {
         [SerializeField] private GhostieDie[] _ghosties;
-        
+        [SerializeField] private GameObject _TOIs;
+        [SerializeField] private Cage _cage;
+
+        private void Start()
+        {
+            _cage.OnGhostEnterCage += GhostieInCage;
+        }
+
         public override void ResetObstacle()
         {
             foreach (var ghostie in _ghosties)
             {
                 ghostie.Live();
             }
+        }
+
+        private void GhostieInCage()
+        {
+            _TOIs.SetActive(false);
         }
     }
 }
